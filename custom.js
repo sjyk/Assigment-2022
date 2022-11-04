8d876031a65c572f4b551a7c31decd6cfed3d562
j2zhao <j2zhao@uchicago.edu>
2022-11-04 00:17:41 -0500

    add files

---
+++ custom.js
@@ -0,0 +1,25 @@
+define([
+    'base/js/namespace',
+    'base/js/events'
+], function(Jupyter, events) {
+    events.on('kernel_ready.Kernel', function() {
+        var cell = Jupyter.notebook.get_cell(0);
+        var prev_text = cell.get_text();
+        if(prev_text.indexOf('%load_ext jupyter_record\n') === -1 ) {
+            var cell = IPython.notebook.insert_cell_above('code');
+            cell.set_text('%load_ext jupyter_record\n%autosave 30\n')
+            Jupyter.notebook.execute_cells([0]);
+        }
+    })
+
+    events.on('create.Cell', function(){
+        var cells = Jupyter.notebook.get_cells();
+        cells.forEach(function(cell) {
+            var prev_text = cell.get_text();
+            if(cell.cell_type == 'code' && prev_text.indexOf('%%git_commit\n') === -1 && prev_text.indexOf('%load_ext jupyter_record\n') === -1) {
+                var text  = '%%git_commit\n' + prev_text;
+                cell.set_text(text);
+            }
+        });
+    });
+});
